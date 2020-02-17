1e26ce9d8b62d46a5c301f380d16b0d9ce063c08
Timmy <r0797009@student.thomasmore.be>
2020-02-17 21:59:51 +0100

    commit

---
+++ TestProjectAzure/TestProjectAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestProjectAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
